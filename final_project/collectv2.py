b2124f2b0f7edc63cc5066b6e3f9c6c11c46f333
Haoqiu Yan <yanhq18@lzu.edu.cn>
2020-06-12 12:39:46 +0800

    Delet print in colect.py.

--- final_project/collectv2.py
+++ final_project/collectv2.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """Collect data"""
 
-import os
 import re
 import pandas as pd
 import time
@@ -22,7 +21,6 @@ def blame_one(kernelRange, fileName, repo, columns):
     time_tail = columns[5]
     rows = [[line[sha_head:sha_tail], line[author_head:author_tail].rstrip(),
              line[time_head:time_tail]] for line in data.decode("utf-8").split("\n") if line]
-    # rows = [[line[:13], line[41:62].rstrip(), line[60:79]] for line in data.decode("utf-8").split("\n") if line]
     return pd.DataFrame(rows, columns=['sha', 'author', 'time_str'])
 
 
@@ -31,36 +29,20 @@ def blame_many(files_columns):
     kernelRange = "v3.0..HEAD"
     rows = []
     for file_path, columns in files_columns.items():
-        # parent_pre = '/'.join(parent.split('\\')[-2:])
-        # filename = filename.replace('\\', '/')
-        # file_path = '/'.join([parent_pre, filename])
-        # file_path = os.path.join(parent, filename).replace('\\', '/')
-        # print("parent", parent)
-        # print("dirnames", dirnames)
-        print("file_path", file_path)
+        print("File_path:\n", file_path)
         df_blame = blame_one(kernelRange, file_path, repo, columns)
         print("df_blame.head(10)", df_blame.head(10))
         the_first_time, the_last_time, average_date, clean_df = count_avetime(df_blame)  # Delete some lines.
-        # print("the_first_time", the_first_time)
-        # print("the_last_time", the_last_time)
-        # print("average_date", average_date)
-        # print("clean_df.head(10)", clean_df.head(10))
         lines = count_lines(clean_df)
         authors = count_author(clean_df)
         shas = count_commit(clean_df)
         fixes_percent, total_shas, last_fixes = gitFixCommits(kernelRange, repo)
-        # print("lines", lines)
-        # print("authors", authors)
-        # print("shas", shas)
-        # print("fixes_percent", fixes_percent)
-        # print("total_shas", total_shas)
-        # print("last_fixes", last_fixes)
         row = {'file_name': file_path, 'lines': lines, 'authors': authors, 'shas': shas,
                'the_first_time': the_first_time, 'the_last_time': the_last_time,
                'average_date': average_date, 'fixes_percent': fixes_percent,
                'total_shas': total_shas, 'last_fixes': last_fixes}
         rows.append(row)
-        print("rows", rows)
+        print("Rows:\n", rows)
     file_info = pd.DataFrame(rows)
     file_info.head(20)
     file_info.to_csv('results.csv', header=True, index=True)
@@ -91,10 +73,7 @@ def count_avetime(df):
             clean_df = df.drop(i, axis=0, inplace=False).reset_index(drop=True)
             bad_time.append(time_str[i])
             pass
-    # print("time_num", time_num)
-    # print("bad_time", bad_time)
     average_date = sum(time_num) / len(time_num)
-    # print("平均时间为", average_date)
     time_num = sorted(time_num)
     the_first_time = time_num[0]
     the_last_time = time_num[-1]
@@ -112,7 +91,7 @@ def list_save(filename, data):  # filename为写入CSV文件的路径，data为�
             line = str(data[i]).replace('[', '').replace(']', '')  # 去除[],这两行按数据不同，可以选择
             line = line.replace("'", '').replace(',', '') + '\n'  # 去除单引号，逗号，每行末尾追加换行符
             fb.write(line)
-    print("Success of saving")
+    print("Filtered lines are saved as BadTime.txt")
 
 
 def gitFixCommits(kernelRange, repo):
@@ -137,24 +116,13 @@ def gitFixCommits(kernelRange, repo):
                 tag = 0
                 print("last_fixes", last_fixes)
             nr_fixes += 1
-            # print(cur_commit[7:19], ",", line.strip()[9:16], sep="")
     fixes_percent = (nr_fixes / total_commits) * 100
-    # print("total found fixes:", nr_fixes)
     return fixes_percent, total_commits, last_fixes
 
 
 if __name__ == '__main__':
-    # work_dir = r'C:\Users\admin\Desktop\linux-stable\kernel\sched'
     files_columns = {"kernel/sched/autogroup.c": [0, 13, 41, 60, 60, 79],
                      "kernel/sched/core.c": [0, 13, 35, 62, 62, 81]}
     blame_many(files_columns)
-    # file_info.head(20)
-    # file_info.to_csv('results.csv', header=True, index=True)
-    # repo = 'C:/Users/admin/Desktop/linux-stable'
-    # kernelRange = "v3.0..HEAD"
-    # fileName = 'kernel/sched/core.c'
-    # df_blame = blame_one(kernelRange, fileName, repo)
-    # print(df_blame.head(10))
-
 
 
