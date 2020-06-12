a9b08247e1dcc4a73335bffabcebd1cf27bee944
Haoqiu Yan <yanhq18@lzu.edu.cn>
2020-06-12 12:33:18 +0800

    Finished collect merely.

--- final_project/collectv2.py
+++ final_project/collectv2.py
@@ -10,53 +10,57 @@ from subprocess import Popen, PIPE
 import unicodedata
 
 
-def blame_one(kernelRange, fileName, repo):
+def blame_one(kernelRange, fileName, repo, columns):
     cmd = ["git", "blame", kernelRange, fileName]
     p = Popen(cmd, cwd=repo, stdout=PIPE)
     data, res = p.communicate()
-    rows = [[line[:13], line[41:62].rstrip(), line[60:79]] for line in data.decode("utf-8").split("\n") if line]
+    sha_head = columns[0]
+    sha_tail = columns[1]
+    author_head = columns[2]
+    author_tail = columns[3]
+    time_head = columns[4]
+    time_tail = columns[5]
+    rows = [[line[sha_head:sha_tail], line[author_head:author_tail].rstrip(),
+             line[time_head:time_tail]] for line in data.decode("utf-8").split("\n") if line]
+    # rows = [[line[:13], line[41:62].rstrip(), line[60:79]] for line in data.decode("utf-8").split("\n") if line]
     return pd.DataFrame(rows, columns=['sha', 'author', 'time_str'])
 
 
-def blame_many(work_dir):
+def blame_many(files_columns):
     repo = 'C:/Users/admin/Desktop/linux-stable'
     kernelRange = "v3.0..HEAD"
     rows = []
-    for parent, dirnames, filenames in os.walk(work_dir, followlinks=True):
-        for filename in filenames:
-            parent_pre = '/'.join(parent.split('\\')[-2:])
-            # filename = filename.replace('\\', '/')
-            file_path = '/'.join([parent_pre, filename])
-            # file_path = os.path.join(parent, filename).replace('\\', '/')
-            # print("parent", parent)
-            # print("dirnames", dirnames)
-            print("file_path", file_path)
-            df_blame = blame_one(kernelRange, file_path, repo)
-            # print("df_blame.head(10)")
-            # print(df_blame.head(10))
-            the_first_time, the_last_time, average_date, clean_df = count_avetime(df_blame)  # Delete some lines.
-            # print("the_first_time", the_first_time)
-            # print("the_last_time", the_last_time)
-            # print("average_date", average_date)
-            # print("clean_df.head(10)", clean_df.head(10))
-            lines = count_lines(clean_df)
-            authors = count_author(clean_df)
-            shas = count_commit(clean_df)
-            fixes_percent, total_shas, last_fixes = gitFixCommits(kernelRange, repo)
-            # print("lines", lines)
-            # print("authors", authors)
-            # print("shas", shas)
-            # print("fixes_percent", fixes_percent)
-            # print("total_shas", total_shas)
-            # print("last_fixes", last_fixes)
-            row = {'file_name': file_path, 'lines': lines, 'authors': authors, 'shas': shas,
-                   'the_first_time': the_first_time, 'the_last_time': the_last_time,
-                   'average_date': average_date, 'fixes_percent': fixes_percent,
-                   'total_shas': total_shas, 'last_fixes': last_fixes}
-            rows.append(row)
-            print("rows", rows)
-            break
-        break
+    for file_path, columns in files_columns.items():
+        # parent_pre = '/'.join(parent.split('\\')[-2:])
+        # filename = filename.replace('\\', '/')
+        # file_path = '/'.join([parent_pre, filename])
+        # file_path = os.path.join(parent, filename).replace('\\', '/')
+        # print("parent", parent)
+        # print("dirnames", dirnames)
+        print("file_path", file_path)
+        df_blame = blame_one(kernelRange, file_path, repo, columns)
+        print("df_blame.head(10)", df_blame.head(10))
+        the_first_time, the_last_time, average_date, clean_df = count_avetime(df_blame)  # Delete some lines.
+        # print("the_first_time", the_first_time)
+        # print("the_last_time", the_last_time)
+        # print("average_date", average_date)
+        # print("clean_df.head(10)", clean_df.head(10))
+        lines = count_lines(clean_df)
+        authors = count_author(clean_df)
+        shas = count_commit(clean_df)
+        fixes_percent, total_shas, last_fixes = gitFixCommits(kernelRange, repo)
+        # print("lines", lines)
+        # print("authors", authors)
+        # print("shas", shas)
+        # print("fixes_percent", fixes_percent)
+        # print("total_shas", total_shas)
+        # print("last_fixes", last_fixes)
+        row = {'file_name': file_path, 'lines': lines, 'authors': authors, 'shas': shas,
+               'the_first_time': the_first_time, 'the_last_time': the_last_time,
+               'average_date': average_date, 'fixes_percent': fixes_percent,
+               'total_shas': total_shas, 'last_fixes': last_fixes}
+        rows.append(row)
+        print("rows", rows)
     file_info = pd.DataFrame(rows)
     file_info.head(20)
     file_info.to_csv('results.csv', header=True, index=True)
@@ -88,9 +92,9 @@ def count_avetime(df):
             bad_time.append(time_str[i])
             pass
     # print("time_num", time_num)
-    print("bad_time", bad_time)
+    # print("bad_time", bad_time)
     average_date = sum(time_num) / len(time_num)
-    print("平均时间为", average_date)
+    # print("平均时间为", average_date)
     time_num = sorted(time_num)
     the_first_time = time_num[0]
     the_last_time = time_num[-1]
@@ -140,8 +144,10 @@ def gitFixCommits(kernelRange, repo):
 
 
 if __name__ == '__main__':
-    work_dir = r'C:\Users\admin\Desktop\linux-stable\kernel\sched'
-    blame_many(work_dir)
+    # work_dir = r'C:\Users\admin\Desktop\linux-stable\kernel\sched'
+    files_columns = {"kernel/sched/autogroup.c": [0, 13, 41, 60, 60, 79],
+                     "kernel/sched/core.c": [0, 13, 35, 62, 62, 81]}
+    blame_many(files_columns)
     # file_info.head(20)
     # file_info.to_csv('results.csv', header=True, index=True)
     # repo = 'C:/Users/admin/Desktop/linux-stable'
