1e84bfb99f483e2c3d61089e1e2a26333bb81d10
Haoqiu Yan <yanhq18@lzu.edu.cn>
2020-06-19 18:28:25 +0800

    Add license

--- final_project/data_collect/collectv3.py
+++ final_project/data_collect/collectv3.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 """Collect data"""
 
+__author__ = "Rob Knight, Gavin Huttley, and Peter Maxwell"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Rob Knight", "Peter Maxwell", "Gavin Huttley",
+                    "Matthew Wakefield"]
+__license__ = "GPL"
+__version__ = "3.0"
+
 import re
 import pandas as pd
 import time
